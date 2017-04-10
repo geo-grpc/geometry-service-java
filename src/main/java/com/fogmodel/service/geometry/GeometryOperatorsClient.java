7d0f6f58843e018f7e012be63dca85b6ae6514ed
David Raleigh <davidraleigh@gmail.com>
2017-04-09 22:09:09 -0700

    first commit based off of google grpc example, because, gradle sucks. java build tools != fun

---
+++ src/main/java/com/fogmodel/service/geometry/GeometryOperatorsClient.java
@@ -0,0 +1,343 @@
+/*
+ * Copyright 2015, Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ *    * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.fogmodel.service.geometry;
+
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.protobuf.Message;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import com.fogmodel.service.geometry.GeometryOperatorsGrpc.GeometryOperatorsBlockingStub;
+import com.fogmodel.service.geometry.GeometryOperatorsGrpc.GeometryOperatorsStub;
+import io.grpc.stub.StreamObserver;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Sample client code that makes gRPC calls to the server.
+ */
+public class GeometryOperatorsClient {
+    private static final Logger logger = Logger.getLogger(GeometryOperatorsClient.class.getName());
+
+    private final ManagedChannel channel;
+    private final GeometryOperatorsBlockingStub blockingStub;
+    private final GeometryOperatorsStub asyncStub;
+
+    private Random random = new Random();
+    private TestHelper testHelper;
+
+    /** Construct client for accessing GeometryOperators server at {@code host:port}. */
+    public GeometryOperatorsClient(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port).usePlaintext(true));
+    }
+
+    /** Construct client for accessing GeometryOperators server using the existing channel. */
+    public GeometryOperatorsClient(ManagedChannelBuilder<?> channelBuilder) {
+        channel = channelBuilder.build();
+        blockingStub = GeometryOperatorsGrpc.newBlockingStub(channel);
+        asyncStub = GeometryOperatorsGrpc.newStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    /**
+     * Blocking unary call example.  Calls getFeature and prints the response.
+     */
+    public void getFeature(int lat, int lon) {
+        info("*** GetFeature: lat={0} lon={1}", lat, lon);
+
+        Point request = Point.newBuilder().setLatitude(lat).setLongitude(lon).build();
+
+        Feature feature;
+        try {
+            feature = blockingStub.getFeature(request);
+            if (testHelper != null) {
+                testHelper.onMessage(feature);
+            }
+        } catch (StatusRuntimeException e) {
+            warning("RPC failed: {0}", e.getStatus());
+            if (testHelper != null) {
+                testHelper.onRpcError(e);
+            }
+            return;
+        }
+        if (GeometryOperatorsUtil.exists(feature)) {
+            info("Found feature called \"{0}\" at {1}, {2}",
+                    feature.getName(),
+                    GeometryOperatorsUtil.getLatitude(feature.getLocation()),
+                    GeometryOperatorsUtil.getLongitude(feature.getLocation()));
+        } else {
+            info("Found no feature at {0}, {1}",
+                    GeometryOperatorsUtil.getLatitude(feature.getLocation()),
+                    GeometryOperatorsUtil.getLongitude(feature.getLocation()));
+        }
+    }
+
+    /**
+     * Blocking server-streaming example. Calls listFeatures with a rectangle of interest. Prints each
+     * response feature as it arrives.
+     */
+    public void listFeatures(int lowLat, int lowLon, int hiLat, int hiLon) {
+        info("*** ListFeatures: lowLat={0} lowLon={1} hiLat={2} hiLon={3}", lowLat, lowLon, hiLat,
+                hiLon);
+
+        Rectangle request =
+                Rectangle.newBuilder()
+                        .setLo(Point.newBuilder().setLatitude(lowLat).setLongitude(lowLon).build())
+                        .setHi(Point.newBuilder().setLatitude(hiLat).setLongitude(hiLon).build()).build();
+        Iterator<Feature> features;
+        try {
+            features = blockingStub.listFeatures(request);
+            for (int i = 1; features.hasNext(); i++) {
+                Feature feature = features.next();
+                info("Result #" + i + ": {0}", feature);
+                if (testHelper != null) {
+                    testHelper.onMessage(feature);
+                }
+            }
+        } catch (StatusRuntimeException e) {
+            warning("RPC failed: {0}", e.getStatus());
+            if (testHelper != null) {
+                testHelper.onRpcError(e);
+            }
+        }
+    }
+
+    /**
+     * Async client-streaming example. Sends {@code numPoints} randomly chosen points from {@code
+     * features} with a variable delay in between. Prints the statistics when they are sent from the
+     * server.
+     */
+    public void recordRoute(List<Feature> features, int numPoints) throws InterruptedException {
+        info("*** RecordRoute");
+        final CountDownLatch finishLatch = new CountDownLatch(1);
+        StreamObserver<RouteSummary> responseObserver = new StreamObserver<RouteSummary>() {
+            @Override
+            public void onNext(RouteSummary summary) {
+                info("Finished trip with {0} points. Passed {1} features. "
+                                + "Travelled {2} meters. It took {3} seconds.", summary.getPointCount(),
+                        summary.getFeatureCount(), summary.getDistance(), summary.getElapsedTime());
+                if (testHelper != null) {
+                    testHelper.onMessage(summary);
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                warning("RecordRoute Failed: {0}", Status.fromThrowable(t));
+                if (testHelper != null) {
+                    testHelper.onRpcError(t);
+                }
+                finishLatch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                info("Finished RecordRoute");
+                finishLatch.countDown();
+            }
+        };
+
+        StreamObserver<Point> requestObserver = asyncStub.recordRoute(responseObserver);
+        try {
+            // Send numPoints points randomly selected from the features list.
+            for (int i = 0; i < numPoints; ++i) {
+                int index = random.nextInt(features.size());
+                Point point = features.get(index).getLocation();
+                info("Visiting point {0}, {1}", GeometryOperatorsUtil.getLatitude(point),
+                        GeometryOperatorsUtil.getLongitude(point));
+                requestObserver.onNext(point);
+                // Sleep for a bit before sending the next one.
+                Thread.sleep(random.nextInt(1000) + 500);
+                if (finishLatch.getCount() == 0) {
+                    // RPC completed or errored before we finished sending.
+                    // Sending further requests won't error, but they will just be thrown away.
+                    return;
+                }
+            }
+        } catch (RuntimeException e) {
+            // Cancel RPC
+            requestObserver.onError(e);
+            throw e;
+        }
+        // Mark the end of requests
+        requestObserver.onCompleted();
+
+        // Receiving happens asynchronously
+        if (!finishLatch.await(1, TimeUnit.MINUTES)) {
+            warning("recordRoute can not finish within 1 minutes");
+        }
+    }
+
+    /**
+     * Bi-directional example, which can only be asynchronous. Send some chat messages, and print any
+     * chat messages that are sent from the server.
+     */
+    public CountDownLatch routeChat() {
+        info("*** RouteChat");
+        final CountDownLatch finishLatch = new CountDownLatch(1);
+        StreamObserver<RouteNote> requestObserver =
+                asyncStub.routeChat(new StreamObserver<RouteNote>() {
+                    @Override
+                    public void onNext(RouteNote note) {
+                        info("Got message \"{0}\" at {1}, {2}", note.getMessage(), note.getLocation()
+                                .getLatitude(), note.getLocation().getLongitude());
+                        if (testHelper != null) {
+                            testHelper.onMessage(note);
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        warning("RouteChat Failed: {0}", Status.fromThrowable(t));
+                        if (testHelper != null) {
+                            testHelper.onRpcError(t);
+                        }
+                        finishLatch.countDown();
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        info("Finished RouteChat");
+                        finishLatch.countDown();
+                    }
+                });
+
+        try {
+            RouteNote[] requests =
+                    {newNote("First message", 0, 0), newNote("Second message", 0, 1),
+                            newNote("Third message", 1, 0), newNote("Fourth message", 1, 1)};
+
+            for (RouteNote request : requests) {
+                info("Sending message \"{0}\" at {1}, {2}", request.getMessage(), request.getLocation()
+                        .getLatitude(), request.getLocation().getLongitude());
+                requestObserver.onNext(request);
+            }
+        } catch (RuntimeException e) {
+            // Cancel RPC
+            requestObserver.onError(e);
+            throw e;
+        }
+        // Mark the end of requests
+        requestObserver.onCompleted();
+
+        // return the latch while receiving happens asynchronously
+        return finishLatch;
+    }
+
+    /** Issues several different requests and then exits. */
+    public static void main(String[] args) throws InterruptedException {
+        List<Feature> features;
+        try {
+            features = GeometryOperatorsUtil.parseFeatures(GeometryOperatorsUtil.getDefaultFeaturesFile());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return;
+        }
+
+        GeometryOperatorsClient client = new GeometryOperatorsClient("localhost", 8980);
+        try {
+            // Looking for a valid feature
+            client.getFeature(409146138, -746188906);
+
+            // Feature missing.
+            client.getFeature(0, 0);
+
+            // Looking for features between 40, -75 and 42, -73.
+            client.listFeatures(400000000, -750000000, 420000000, -730000000);
+
+            // Record a few randomly selected points from the features file.
+            client.recordRoute(features, 10);
+
+            // Send and receive some notes.
+            CountDownLatch finishLatch = client.routeChat();
+
+            if (!finishLatch.await(1, TimeUnit.MINUTES)) {
+                client.warning("routeChat can not finish within 1 minutes");
+            }
+        } finally {
+            client.shutdown();
+        }
+    }
+
+    private void info(String msg, Object... params) {
+        logger.log(Level.INFO, msg, params);
+    }
+
+    private void warning(String msg, Object... params) {
+        logger.log(Level.WARNING, msg, params);
+    }
+
+    private RouteNote newNote(String message, int lat, int lon) {
+        return RouteNote.newBuilder().setMessage(message)
+                .setLocation(Point.newBuilder().setLatitude(lat).setLongitude(lon).build()).build();
+    }
+
+    /**
+     * Only used for unit test, as we do not want to introduce randomness in unit test.
+     */
+    @VisibleForTesting
+    void setRandom(Random random) {
+        this.random = random;
+    }
+
+    /**
+     * Only used for helping unit test.
+     */
+    @VisibleForTesting
+    interface TestHelper {
+        /**
+         * Used for verify/inspect message received from server.
+         */
+        void onMessage(Message message);
+
+        /**
+         * Used for verify/inspect error received from server.
+         */
+        void onRpcError(Throwable exception);
+    }
+
+    @VisibleForTesting
+    void setTestHelper(TestHelper testHelper) {
+        this.testHelper = testHelper;
+    }
+}
---
+++ src/main/java/com/fogmodel/service/geometry/GeometryOperatorsServer.java
@@ -0,0 +1,287 @@
+package com.fogmodel.service.geometry;
+
+import static java.lang.Math.atan2;
+import static java.lang.Math.cos;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.toRadians;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import com.fogmodel.service.geometry.*;
+
+import io.grpc.stub.StreamObserver;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by davidraleigh on 4/9/17.
+ */
+public class GeometryOperatorsServer {
+    private static final Logger logger = Logger.getLogger(GeometryOperatorsServer.class.getName());
+
+    private final int port;
+    private final Server server;
+
+    public GeometryOperatorsServer(int port) throws IOException {
+        this(port, GeometryOperatorsUtil.getDefaultFeaturesFile());
+    }
+
+    /** Create a GeometryOperators server listening on {@code port} using {@code featureFile} database. */
+    public GeometryOperatorsServer(int port, URL featureFile) throws IOException {
+        this(ServerBuilder.forPort(port), port, GeometryOperatorsUtil.parseFeatures(featureFile));
+    }
+
+    /** Create a GeometryOperators server using serverBuilder as a base and features as data. */
+    public GeometryOperatorsServer(ServerBuilder<?> serverBuilder, int port, Collection<Feature> features) {
+        this.port = port;
+        server = serverBuilder.addService(new GeometryOperatorsService(features))
+                .build();
+    }
+
+    /** Start serving requests. */
+    public void start() throws IOException {
+        server.start();
+        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may has been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                GeometryOperatorsServer.this.stop();
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    /** Stop serving requests and shutdown resources. */
+    public void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    /**
+     * Main method.  This comment makes the linter happy.
+     */
+    public static void main(String[] args) throws Exception {
+        GeometryOperatorsServer server = new GeometryOperatorsServer(8980);
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    /**
+     * Our implementation of GeometryOperators service.
+     *
+     * <p>See route_guide.proto for details of the methods.
+     */
+    private static class GeometryOperatorsService extends GeometryOperatorsGrpc.GeometryOperatorsImplBase {
+        private final Collection<Feature> features;
+        private final ConcurrentMap<Point, List<RouteNote>> routeNotes =
+                new ConcurrentHashMap<Point, List<RouteNote>>();
+
+        GeometryOperatorsService(Collection<Feature> features) {
+            this.features = features;
+        }
+
+        /**
+         * Gets the {@link Feature} at the requested {@link Point}. If no feature at that location
+         * exists, an unnamed feature is returned at the provided location.
+         *
+         * @param request the requested location for the feature.
+         * @param responseObserver the observer that will receive the feature at the requested point.
+         */
+        @Override
+        public void getFeature(Point request, StreamObserver<Feature> responseObserver) {
+            responseObserver.onNext(checkFeature(request));
+            responseObserver.onCompleted();
+        }
+
+        /**
+         * Gets all features contained within the given bounding {@link Rectangle}.
+         *
+         * @param request the bounding rectangle for the requested features.
+         * @param responseObserver the observer that will receive the features.
+         */
+        @Override
+        public void listFeatures(Rectangle request, StreamObserver<Feature> responseObserver) {
+            int left = min(request.getLo().getLongitude(), request.getHi().getLongitude());
+            int right = max(request.getLo().getLongitude(), request.getHi().getLongitude());
+            int top = max(request.getLo().getLatitude(), request.getHi().getLatitude());
+            int bottom = min(request.getLo().getLatitude(), request.getHi().getLatitude());
+
+            for (Feature feature : features) {
+                if (!GeometryOperatorsUtil.exists(feature)) {
+                    continue;
+                }
+
+                int lat = feature.getLocation().getLatitude();
+                int lon = feature.getLocation().getLongitude();
+                if (lon >= left && lon <= right && lat >= bottom && lat <= top) {
+                    responseObserver.onNext(feature);
+                }
+            }
+            responseObserver.onCompleted();
+        }
+
+        /**
+         * Gets a stream of points, and responds with statistics about the "trip": number of points,
+         * number of known features visited, total distance traveled, and total time spent.
+         *
+         * @param responseObserver an observer to receive the response summary.
+         * @return an observer to receive the requested route points.
+         */
+        @Override
+        public StreamObserver<Point> recordRoute(final StreamObserver<RouteSummary> responseObserver) {
+            return new StreamObserver<Point>() {
+                int pointCount;
+                int featureCount;
+                int distance;
+                Point previous;
+                final long startTime = System.nanoTime();
+
+                @Override
+                public void onNext(Point point) {
+                    pointCount++;
+                    if (GeometryOperatorsUtil.exists(checkFeature(point))) {
+                        featureCount++;
+                    }
+                    // For each point after the first, add the incremental distance from the previous point to
+                    // the total distance value.
+                    if (previous != null) {
+                        distance += calcDistance(previous, point);
+                    }
+                    previous = point;
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    logger.log(Level.WARNING, "recordRoute cancelled");
+                }
+
+                @Override
+                public void onCompleted() {
+                    long seconds = NANOSECONDS.toSeconds(System.nanoTime() - startTime);
+                    responseObserver.onNext(RouteSummary.newBuilder().setPointCount(pointCount)
+                            .setFeatureCount(featureCount).setDistance(distance)
+                            .setElapsedTime((int) seconds).build());
+                    responseObserver.onCompleted();
+                }
+            };
+        }
+
+        /**
+         * Receives a stream of message/location pairs, and responds with a stream of all previous
+         * messages at each of those locations.
+         *
+         * @param responseObserver an observer to receive the stream of previous messages.
+         * @return an observer to handle requested message/location pairs.
+         */
+        @Override
+        public StreamObserver<RouteNote> routeChat(final StreamObserver<RouteNote> responseObserver) {
+            return new StreamObserver<RouteNote>() {
+                @Override
+                public void onNext(RouteNote note) {
+                    List<RouteNote> notes = getOrCreateNotes(note.getLocation());
+
+                    // Respond with all previous notes at this location.
+                    for (RouteNote prevNote : notes.toArray(new RouteNote[0])) {
+                        responseObserver.onNext(prevNote);
+                    }
+
+                    // Now add the new note to the list
+                    notes.add(note);
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    logger.log(Level.WARNING, "routeChat cancelled");
+                }
+
+                @Override
+                public void onCompleted() {
+                    responseObserver.onCompleted();
+                }
+            };
+        }
+
+        /**
+         * Get the notes list for the given location. If missing, create it.
+         */
+        private List<RouteNote> getOrCreateNotes(Point location) {
+            List<RouteNote> notes = Collections.synchronizedList(new ArrayList<RouteNote>());
+            List<RouteNote> prevNotes = routeNotes.putIfAbsent(location, notes);
+            return prevNotes != null ? prevNotes : notes;
+        }
+
+        /**
+         * Gets the feature at the given point.
+         *
+         * @param location the location to check.
+         * @return The feature object at the point. Note that an empty name indicates no feature.
+         */
+        private Feature checkFeature(Point location) {
+            for (Feature feature : features) {
+                if (feature.getLocation().getLatitude() == location.getLatitude()
+                        && feature.getLocation().getLongitude() == location.getLongitude()) {
+                    return feature;
+                }
+            }
+
+            // No feature was found, return an unnamed feature.
+            return Feature.newBuilder().setName("").setLocation(location).build();
+        }
+
+        /**
+         * Calculate the distance between two points using the "haversine" formula.
+         * This code was taken from http://www.movable-type.co.uk/scripts/latlong.html.
+         *
+         * @param start The starting point
+         * @param end The end point
+         * @return The distance between the points in meters
+         */
+        private static int calcDistance(Point start, Point end) {
+            double lat1 = GeometryOperatorsUtil.getLatitude(start);
+            double lat2 = GeometryOperatorsUtil.getLatitude(end);
+            double lon1 = GeometryOperatorsUtil.getLongitude(start);
+            double lon2 = GeometryOperatorsUtil.getLongitude(end);
+
+
+
+            int r = 6371000; // meters
+            double phi1 = toRadians(lat1);
+            double phi2 = toRadians(lat2);
+            double deltaPhi = toRadians(lat2 - lat1);
+            double deltaLambda = toRadians(lon2 - lon1);
+
+            double a = sin(deltaPhi / 2) * sin(deltaPhi / 2)
+                    + cos(phi1) * cos(phi2) * sin(deltaLambda / 2) * sin(deltaLambda / 2);
+            double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+
+            return (int) (r * c);
+        }
+    }
+
+}
+
+
