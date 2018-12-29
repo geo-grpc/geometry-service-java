0e032536454ac39cdfa5bcb2ed0f78192321f476
David Raleigh <david@swiftera.co>
2018-12-28 17:24:18 -0800

    fix

--- java-image-upload.sh
+++ java-image-upload.sh
@@ -21,12 +21,30 @@ echo $6
 echo Dockerfile name 7
 echo $7
 
+echo docker build --no-cache --rm=true --quiet --build-arg JDK_TAG=$5 --build-arg JRE_TAG=$6 -t $2:$6-build-$1 -f $7 .
 docker build --no-cache --rm=true --quiet --build-arg JDK_TAG=$5 --build-arg JRE_TAG=$6 -t $2:$6-build-$1 -f $7 .
+
+echo docker push $2:$6-build-$1
 docker push $2:$6-build-$1
+
+echo docker tag $2:$6-build-$1 $2:$6
 docker tag $2:$6-build-$1 $2:$6
+
+echo docker push $2:$6
 docker push $2:$6
-docker tag $2:$6 $3/$2:$6
-docker push $3/$2:$6
-docker rmi $3/$2:$6
+
+echo docker tag $2:$6 $3/$4:$6
+docker tag $2:$6 $3/$4:$6
+
+echo docker push $3/$4:$6
+docker push $3/$4:$6
+
+
+echo docker rmi $3/$4:$6
+docker rmi $3/$4:$6
+
+echo docker rmi $2:$6
 docker rmi $2:$6
+
+echo docker rmi $2:$6-build-$1
 docker rmi $2:$6-build-$1
