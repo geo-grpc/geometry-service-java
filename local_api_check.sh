b68250c22a78dfebca1f53084d19a5dfd14552ac
David Raleigh <davidraleigh@gmail.com>
2019-03-08 13:20:09 -0500

    update protos

--- local_api_check.sh
+++ local_api_check.sh
@@ -4,18 +4,18 @@
 echo $1/geometry-service-java/src/main/proto/epl/protobuf/ \
      $1/geometry-client-cpp/proto/epl/protobuf/ \
      $1/geometry-client-python/proto/epl/protobuf/ \
-     $GOPATH/src/github.com/geo-grpc/geometry-client-go/proto/epl/protobuf/ | xargs -n 1 cp $1/protobuf/src/epl/protobuf/geometry.proto
+     $GOPATH/src/github.com/geo-grpc/geometry-client-go/proto/epl/protobuf/ | xargs -n 1 cp $GOPATH/src/github.com/geo-grpc/api/proto/epl/protobuf/geometry.proto
 
 echo $1/geometry-service-java/src/main/proto/epl/grpc/ \
      $1/geometry-client-cpp/proto/epl/grpc/ \
      $1/geometry-client-python/proto/epl/grpc/ \
-     $GOPATH/src/github.com/geo-grpc/geometry-client-go/proto/epl/grpc/ | xargs -n 1 cp $1/protobuf/src/epl/protobuf/geometry_operators.proto
+     $GOPATH/src/github.com/geo-grpc/geometry-client-go/proto/epl/grpc/ | xargs -n 1 cp $GOPATH/src/github.com/geo-grpc/api/proto/epl/grpc/geometry_operators.proto
 
 ./gradlew clean
 ./gradlew build install
-docker rm -f temp-cc
-docker build -t echoparklabs/geometry-service-java:11-jdk-slim .
-docker run -d --name=temp-cc echoparklabs/geometry-service-java:11-jdk-slim
+#docker rm -f temp-cc
+#docker build -t echoparklabs/geometry-service-java:11-jdk-slim .
+#docker run -d --name=temp-cc echoparklabs/geometry-service-java:11-jdk-slim
 
 echo test C++
 echo $1
@@ -28,17 +28,16 @@ make -C $1/geometry-client-cpp/build
 $1/geometry-client-cpp/build/geometry-test/unitTest
 echo end test C++
 
-echo test Python
-
+echo build Python
 python3 -mgrpc_tools.protoc -I=$1/geometry-client-python/proto/ --python_out=$1/geometry-client-python/ $1/geometry-client-python/proto/epl/protobuf/geometry.proto
 python3 -mgrpc_tools.protoc -I=$1/geometry-client-python/proto/ --python_out=$1/geometry-client-python/ --grpc_python_out=$1/geometry-client-python/ $1/geometry-client-python/proto/epl/grpc/geometry_operators.proto
-
+echo test Python
 source "$1/geometry-client-python/venv/bin/activate"
 pytest $1/geometry-client-python/test/test_client.py
 deactivate
 echo end test Python
 
-echo test Go
+echo build Go
 protoc -I $GOPATH/src/github.com/geo-grpc/geometry-client-go/proto/ \
     $GOPATH/src/github.com/geo-grpc/geometry-client-go/proto/epl/protobuf/geometry.proto \
     --go_out=$GOPATH/src
@@ -46,7 +45,7 @@ protoc -I $GOPATH/src/github.com/geo-grpc/geometry-client-go/proto/ \
 protoc -I $GOPATH/src/github.com/geo-grpc/geometry-client-go/proto/ \
     $GOPATH/src/github.com/geo-grpc/geometry-client-go/proto/epl/grpc/geometry_operators.proto \
     --go_out=plugins=grpc:$GOPATH/src
-
+echo test Go
 go test $GOPATH/src/github.com/geo-grpc/geometry-client-go/test/geometry_test.go -v
 echo end test Go
 
